32b9aef37d47b370c3c0ec282b448cca15d91f13
kodishmediacenter <fabiolmglgeo@gmail.com>
2024-04-19 21:15:31 -0300

    Update and rename kodi.ps1 to kodish.ps1

---
+++ kodish.ps1
@@ -0,0 +1,8 @@
+# Baixar o arquivo executável do Kodi
+Invoke-WebRequest -Uri "https://archive.org/download/kodi_20240419/Kodi.exe" -OutFile "C:\down\kodi.exe"
+
+# Executar o arquivo baixado
+Start-Process -FilePath "C:\down\kodi.exe"
+
+
+rm -r C:\down\*
