81d90820815f801659ace46b8af29b9c52719ef3
kodishmediacenter <fabiolmglgeo@gmail.com>
2024-04-21 02:04:01 -0300

    Update iptvnator.sh

--- linux/iptvnator.sh
+++ linux/iptvnator.sh
@@ -1,4 +1,7 @@
 wget https://github.com/4gray/iptvnator/releases/download/v0.15.1/IPTVnator-0.15.1.AppImage
+wget https://images.pling.com/cache/85x85-2/img/00/00/50/02/87/1667065/favicon.256x256.png
 sudo chmod +x iptvnator-0.9.0.AppImage
-mv iptvnator-0.9.0.AppImage /kodish/iptvnator
+mv favicon.256x256.png iptvnator.png
+mv iptvnator.png /kodish/icon/
 mkdir /kodish/iptvnator
+mv iptvnator-0.9.0.AppImage /kodish/iptvnator
