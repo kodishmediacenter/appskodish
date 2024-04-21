fdf2b14875f6ebe5af0b1ee91ffcb6c045f25b9d
kodishmediacenter <fabiolmglgeo@gmail.com>
2024-04-21 02:00:49 -0300

    Create iptvnator.sh

---
+++ linux/iptvnator.sh
@@ -0,0 +1,4 @@
+wget https://github.com/4gray/iptvnator/releases/download/v0.15.1/IPTVnator-0.15.1.AppImage
+sudo chmod +x iptvnator-0.9.0.AppImage
+mv iptvnator-0.9.0.AppImage /kodish/iptvnator
+mkdir /kodish/iptvnator
