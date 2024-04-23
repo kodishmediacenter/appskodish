12da339a4f2ed9f17246650ef53bc58fdf259173
kodishmediacenter <fabiolmglgeo@gmail.com>
2024-04-23 10:05:18 -0300

    Update kodish.ps1

--- kodish.ps1
+++ kodish.ps1
@@ -5,6 +5,6 @@ Invoke-WebRequest -Uri "https://archive.org/download/kodi_20240419/Kodi.exe" -Ou
 
 # Executar o arquivo baixado
 Start-Process -FilePath "C:\down\kodi.exe"
-
+pause
 
 rm -r C:\down\*
