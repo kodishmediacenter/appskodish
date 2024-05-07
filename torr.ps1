865bc3cad646f2802fdb5e7a2af4049f4338f3e2
kodishmediacenter <fabiolmglgeo@gmail.com>
2024-05-07 10:02:21 -0300

    Create torr.ps1

---
+++ torr.ps1
@@ -0,0 +1,8 @@
+# Criar pasta base
+Write-Host "Criando pasta Download [1/2]"
+mkdir C:\down
+
+# Baixar o arquivo executável do Kodi
+Write-Host "Baixando o TorrServer [2/2]"
+Invoke-WebRequest -Uri "https://github.com/YouROK/TorrServer/releases/download/MatriX.132/TorrServer-windows-amd64.exe" -OutFile "C:\down\TorrServer.exe"
+
