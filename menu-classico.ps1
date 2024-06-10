dd1c339478d6d172a97083aa7ccfe90f00141a8b
kodishmediacenter <fabiolmglgeo@gmail.com>
2024-06-10 10:00:47 -0300

    Update menu-classico.ps1

--- menu-classico.ps1
+++ menu-classico.ps1
@@ -4,7 +4,8 @@ mkdir C:\down
 
 # Baixar o arquivo script menu classico
 Write-Host "Baixando o script reativar menu classico [2/3]"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/kodishmediacenter/appskodish/main/menu-classico.bat" -OutFile "menu-classico.bat"
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/kodishmediacenter/appskodish/main/menu-classico.bat -OutFile "menu-classico.bat"
+
 
 # Executar o arquivo baixado
 Write-Host "Executar o arquivo  [3/3]"
