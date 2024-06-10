5bbec88f216f3b82c4cabff6d8eb8a83744a5036
kodishmediacenter <fabiolmglgeo@gmail.com>
2024-06-10 10:07:23 -0300

    Update and rename menu-classico1.ps1 to menu-classico2.ps1

--- menu-classico2.ps1
+++ menu-classico2.ps1
@@ -4,9 +4,9 @@ mkdir C:\down
 
 # Baixar o arquivo script menu classico
 Write-Host "Baixando o script reativar menu classico [2/3]"
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/kodishmediacenter/appskodish/main/menu-classico.bat -OutFile "menu-classico.bat"
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/kodishmediacenter/appskodish/main/menu-classico.cmd -OutFile "menu-classico.cmd"
 
 
 # Executar o arquivo baixado
 Write-Host "Executar o arquivo  [3/3]"
-Start-Process -FilePath "C:\down\menu-classico.bat"
+Start-Process -FilePath "C:\down\menu-classico.cmd"
