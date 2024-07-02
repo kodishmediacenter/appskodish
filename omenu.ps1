0e3ffa271a59d62f6ae9a7ef2092ecb2abada8f3
kodishmediacenter <fabiolmglgeo@gmail.com>
2024-07-02 18:18:36 -0300

    Update omenu.ps1

--- omenu.ps1
+++ omenu.ps1
@@ -1,25 +1,28 @@
+Aqui está o código corrigido para exibir e executar a seleção das opções em português brasileiro:
+
+```powershell
 Clear-Host
 Write-Host "========================================================================================================================================="
 Write-Host "======================== Kodish Store Para Windows ======================================================================================"
 Write-Host "========================================================================================================================================="
 Write-Host "`n"
-write-Host "1 Kodi `n" 
-write-Host "2 Kodi da Kodish `n "
-write-Host "3 Torr Server (64 bits) `n" 
-write-Host "4 Torr Server (32 bits) `n" 
-write-Host "5 Win Boot (Sayro Digital e Duanny) `n"
-write-Host "6 Fix Internet (Sayro Digital e Duanny) `n" 
-write-Host "7 Test Defrag Disk (Sayro Digital e Duanny) `n"
-write-Host "8 Baixar Winget `n"
-write-Host "9 Instalar Wmic para Windows 11 24H2 `n"
-write-Host "10 Voltar menu do botão direito do mouse classico (win 11) `n"
-write-Host "11 Baixar Aomei Partition para auxiliar nas partições do seu Computador  `n"
-write-Host "12 Acessar Apps para Download  `n"
-write-Host "13 Acessar EXM Tweeks  `n"
-write-Host "14 Acessar Win 11 Debloat  `n"
-write-Host "15 Instalar Microsoft Office `n"
-write-Host "100 Remover Pasta de Download`n"
-write-Host "101 Reiniciar o Explorer"
+Write-Host "1 Kodi `n" 
+Write-Host "2 Kodi da Kodish `n"
+Write-Host "3 Torr Server (64 bits) `n" 
+Write-Host "4 Torr Server (32 bits) `n" 
+Write-Host "5 Win Boot (Sayro Digital e Duanny) `n"
+Write-Host "6 Fix Internet (Sayro Digital e Duanny) `n" 
+Write-Host "7 Test Defrag Disk (Sayro Digital e Duanny) `n"
+Write-Host "8 Baixar Winget `n"
+Write-Host "9 Instalar Wmic para Windows 11 24H2 `n"
+Write-Host "10 Voltar menu do botão direito do mouse clássico (Win 11) `n"
+Write-Host "11 Baixar Aomei Partition para auxiliar nas partições do seu Computador  `n"
+Write-Host "12 Acessar Apps para Download  `n"
+Write-Host "13 Acessar EXM Tweaks  `n"
+Write-Host "14 Acessar Win 11 Debloat  `n"
+Write-Host "15 Instalar Microsoft Office `n"
+Write-Host "100 Remover Pasta de Download `n"
+Write-Host "101 Reiniciar o Explorer `n"
 $opcao = Read-Host "`n Escolha uma Opção:"
 
 switch ($opcao) {
@@ -31,68 +34,67 @@ switch ($opcao) {
         Write-Host "Iniciar a Instalação do Kodi da Kodish"
         irm https://raw.githubusercontent.com/kodishmediacenter/appskodish/main/kodish.ps1 | iex
     }
-    '3'{
-        Write-Host "Baixar Torr Server for Windows 64 bits"
+    '3' {
+        Write-Host "Baixar Torr Server para Windows 64 bits"
         irm https://raw.githubusercontent.com/kodishmediacenter/appskodish/main/torr.ps1 | iex
     }
-    '4'{
-        Write-Host "Baixar Torr Server for Windows 32 bits"
+    '4' {
+        Write-Host "Baixar Torr Server para Windows 32 bits"
         irm https://raw.githubusercontent.com/kodishmediacenter/appskodish/main/torr32bits.ps1 | iex
     }
-    
-    '5'{
-        Write-Host "Baixar Win boot (Sayro DIgital e Duanny)"
+    '5' {
+        Write-Host "Baixar Win Boot (Sayro Digital e Duanny)"
         irm https://raw.githubusercontent.com/kodishmediacenter/appskodish/main/winboot.ps1 | iex
     }
-	'6'{
-        Write-Host "Baixar Fix Internet (Sayro DIgital e Duanny)"
+    '6' {
+        Write-Host "Baixar Fix Internet (Sayro Digital e Duanny)"
         irm https://raw.githubusercontent.com/kodishmediacenter/appskodish/main/Fixinternet.ps1 | iex
     }
-	'7'{
-        Write-Host "Baixar Test Defrag Disk (Sayro DIgital e Duanny)"
+    '7' {
+        Write-Host "Baixar Test Defrag Disk (Sayro Digital e Duanny)"
         irm https://raw.githubusercontent.com/kodishmediacenter/appskodish/main/TestDefragDisk.ps1 | iex
     }
-    	'8'{
-    	write-Host "Baixando Winget Script"
-     	irm https://raw.githubusercontent.com/kodishmediacenter/appskodish/main/winget.ps1 | iex
+    '8' {
+        Write-Host "Baixando Winget Script"
+        irm https://raw.githubusercontent.com/kodishmediacenter/appskodish/main/winget.ps1 | iex
     }
-    	'9'{
-     	write-Host "Baixando Wmic para Windows 11"
-      	irm https://raw.githubusercontent.com/kodishmediacenter/appskodish/main/wmic.ps1 | iex     
-   }
-   	'10'{
-        write-Host "Baixando script do menu classico do botão direito do mouse windows 11"
-   	irm  https://raw.githubusercontent.com/kodishmediacenter/appskodish/main/menu-classico1.ps1 | iex 
-  }
-     	'11'{
-        write-Host "Baixando Aomei Partition"
-   	irm  https://raw.githubusercontent.com/kodishmediacenter/appskodish/main/aomei.ps1 | iex 
-  }
-  	'12'{
-        write-Host "Acessando Ninite Apps"
-   	irm  https://raw.githubusercontent.com/kodishmediacenter/appskodish/main/ninite2.ps1 | iex 
-  }
-    	'13'{
-        write-Host "Acessando Ninite Apps"
-   	irm https://raw.githubusercontent.com/kodishmediacenter/appskodish/main/exm.ps1 | iex 
-  }
-  	'14'{
-   	& ([scriptblock]::Create((irm "https://raw.githubusercontent.com/Raphire/Win11Debloat/master/Get.ps1")))
-  }
-  }
-        '15'{
-        write-Host "Acessando Office"
-   	irm https://raw.githubusercontent.com/kodishmediacenter/appskodish/main/officeS1.ps1 | iex 
-  }
-       '100' {
-        Write-Host "Limpar a Pasta de Trabalho"
+    '9' {
+        Write-Host "Baixando Wmic para Windows 11"
+        irm https://raw.githubusercontent.com/kodishmediacenter/appskodish/main/wmic.ps1 | iex     
+    }
+    '10' {
+        Write-Host "Baixando script do menu clássico do botão direito do mouse para Windows 11"
+        irm https://raw.githubusercontent.com/kodishmediacenter/appskodish/main/menu-classico1.ps1 | iex 
+    }
+    '11' {
+        Write-Host "Baixando Aomei Partition"
+        irm https://raw.githubusercontent.com/kodishmediacenter/appskodish/main/aomei.ps1 | iex 
+    }
+    '12' {
+        Write-Host "Acessando Ninite Apps"
+        irm https://raw.githubusercontent.com/kodishmediacenter/appskodish/main/ninite2.ps1 | iex 
+    }
+    '13' {
+        Write-Host "Acessando EXM Tweaks"
+        irm https://raw.githubusercontent.com/kodishmediacenter/appskodish/main/exm.ps1 | iex 
+    }
+    '14' {
+        & ([scriptblock]::Create((irm "https://raw.githubusercontent.com/Raphire/Win11Debloat/master/Get.ps1")))
+    }
+    '15' {
+        Write-Host "Acessando Office"
+        irm https://raw.githubusercontent.com/kodishmediacenter/appskodish/main/officeS1.ps1 | iex 
+    }
+    '100' {
+        Write-Host "Limpar a Pasta de Downloads"
         Remove-Item -Path "C:\down\" -Recurse -Force
     }
-    	'101'{
-     	Stop-Process -Name explorer -Force
-      	Start-Process explorer
-     }
+    '101' {
+        Stop-Process -Name explorer -Force
+        Start-Process explorer
+    }
     Default {
         Write-Host "Opção inválida."
     }
 }
+```
