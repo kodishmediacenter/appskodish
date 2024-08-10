8d83a13584b998abe2509d7a8b19c6889ed87ef9
kodishmediacenter <fabiolmglgeo@gmail.com>
2024-08-10 15:58:19 -0300

    Update windows-switcher.ps1

--- windows-switcher.ps1
+++ windows-switcher.ps1
@@ -3,8 +3,8 @@ Write-Host "Criando pasta Download [1/2]"
 mkdir C:\down
 
 # Baixar o arquivo executável do Winboot
-Write-Host "Baixando o Aomei Partition [2/2]"
+Write-Host "Baixando o Windows Edition Switcher [2/2]"
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/kodishmediacenter/appskodish/main/Windows-Edition-Switcher.cmd" -OutFile "C:\down\WESwitcher.cmd"
 
 Write-Host "Download Comcluido para Continuar necessario ir a Pasta C:\down necessario extrair arquivo zip [3/3]"
-Start-Process -FilePath "WESwitcher.cmd"
+Start-Process -FilePath "C:\down\WESwitcher.cmd"
