458514ae402ba67a5759e07970c2dcf098441832
kodishmediacenter <fabiolmglgeo@gmail.com>
2024-08-13 11:53:03 -0300

    Update and rename hirensboot.ps1 to hirensboot1.ps1

--- hirensboot1.ps1
+++ hirensboot1.ps1
@@ -3,4 +3,5 @@ Write-Host "Criando pasta Download [1/3]"
 mkdir C:\down
 # Baixar o Hirens Boot (Linorg)
 Write-Host "Baixando o Kodi da kodish [2/3]"
-Invoke-WebRequest -Uri "https://linorg.usp.br/hbcd/HBCD_PE_x64.iso" -OutFile "C:\down\HBCD_PE_x64.iso"
+Start-BitsTransfer -Source "https://linorg.usp.br/hbcd/HBCD_PE_x64.iso" -Destination "C:\down\HBCD_PE_x64.iso"
+
