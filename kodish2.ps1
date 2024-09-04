b90f341f376630f1ba0f6f8ed3cc1f6ae53e9f89
kodishmediacenter <fabiolmglgeo@gmail.com>
2024-09-04 13:44:00 -0300

    Create kodish2.ps1

---
+++ kodish2.ps1
@@ -0,0 +1,10 @@
+# Criar pasta base
+Write-Host "Criando pasta Download [1/3]"
+mkdir C:\down
+# Baixar o arquivo executável do Kodi
+Write-Host "Baixando o Kodi da kodish [2/3]"
+Invoke-WebRequest -Uri "https://archive.org/download/kodi_20240904/Kodi.exe" -OutFile "C:\down\kodi.exe"
+
+# Executar o arquivo baixado
+Write-Host "Iniciando a Inslação do Kodi da Kodish [3/3]"
+Start-Process -FilePath "C:\down\kodi.exe"
