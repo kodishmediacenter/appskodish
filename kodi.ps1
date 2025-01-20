67dad114de7e92d31bb21b10489912fd9a4d2966
kodishmediacenter <fabiolmglgeo@gmail.com>
2025-01-20 10:00:56 -0300

    Update kodi.ps1

--- kodi.ps1
+++ kodi.ps1
@@ -2,8 +2,8 @@
 Write-Host "Criando pasta Download [1/3]"
 mkdir C:\down
 # Baixar o arquivo executável do Kodi
-Write-Host "Baixando o Kodi 21.0 [2/3]"
-Invoke-WebRequest -Uri "https://mirrors.kodi.tv/releases/windows/win64/kodi-21.0-Omega-x64.exe" -OutFile "C:\down\kodi.exe"
+Write-Host "Baixando o Kodi 21.2 [2/3]"
+Invoke-WebRequest -Uri "https://kodi.mirror.garr.it/releases/windows/win64/kodi-21.2-Omega-x64.exe" -OutFile "C:\down\kodi.exe"
 
 # Executar o arquivo baixado
 Write-Host "Iniciando a Inslação do Kodi 21.0  [3/3]"
