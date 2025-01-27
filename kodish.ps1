c87b2cbce7a53128d9804632c263b9b5efbd4f78
kodishmediacenter <fabiolmglgeo@gmail.com>
2025-01-27 11:36:34 -0300

    Update kodish.ps1

--- kodish.ps1
+++ kodish.ps1
@@ -3,7 +3,7 @@ Write-Host "Criando pasta Download [1/3]"
 mkdir C:\down
 # Baixar o arquivo executável do Kodi
 Write-Host "Baixando o Kodi da kodish [2/3]"
-Invoke-WebRequest -Uri "https://archive.org/download/kodi_20240419/Kodi.exe" -OutFile "C:\down\kodi.exe"
+Invoke-WebRequest -Uri "https://archive.org/download/kodi_20250127/Kodi.exe" -OutFile "C:\down\kodi.exe"
 
 # Executar o arquivo baixado
 Write-Host "Iniciando a Inslação do Kodi da Kodish [3/3]"
