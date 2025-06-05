325be42956c9a12f8310c684bca7bb2f083453a7
kodishmediacenter <fabiolmglgeo@gmail.com>
2025-06-05 14:10:50 -0300

    Update Kodimod.ps1

--- Kodimod.ps1
+++ Kodimod.ps1
@@ -6,5 +6,5 @@ Write-Host "Baixando o Kodi 21.2.3 mod by Kodish  [2/3]"
 Invoke-WebRequest -Uri "https://archive.org/download/kodi_20250605/Kodi.exe" -OutFile "C:\down\Kodi.exe"
 
 # Executar o arquivo baixado
-Write-Host "Iniciando a Inslação do Kodi 21.0  [3/3]"
+Write-Host "Iniciando a Instalação do Kodi 21.0  [3/3]"
 Start-Process -FilePath "C:\down\Kodi.exe"
